49be5754ea64b20e277ccab85d786291f9e58a5a
rafael garcia <sfrafaelgarcia@gmail.com>
2024-12-04 13:08:53 -0800

    Initial Commit

---
+++ OrgStructure/OrgStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrgStructureApp.swift
+//  OrgStructure
+//
+//  Created by Rafael Garcia on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OrgStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrgStructureTests/OrgStructureTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrgStructureTests.swift
+//  OrgStructureTests
+//
+//  Created by Rafael Garcia on 12/4/24.
+//
+
+import Testing
+@testable import OrgStructure
+
+struct OrgStructureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
